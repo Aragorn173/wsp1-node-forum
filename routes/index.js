b022f90bceee2bbb7d5d3b98a112ae1fc12d7ee6
Aragorn173 <alvin.liljestrom@elev.ga.ntig.se>
2023-01-31 14:17:24 +0100

    sidan funkar

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ const promisePool = pool.promise();
 
 
 router.get('/', async function (req, res, next) {
-    const [rows] = await promisePool.query("SELECT * FROM al04forum");
+    const [rows] = await promisePool.query("SELECT * FROM al04forum ORDER BY createdAt DESC");
     res.render('index.njk', {
         rows: rows,
         title: 'Forum',
@@ -24,8 +24,20 @@ router.get('/', async function (req, res, next) {
 
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO al04forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
+
+    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
+    let [user] = await promisePool.query('SELECT * FROM al04users WHERE id = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO al04users (name) VALUES (?)', [author]);
+    }
+
+    // user.insertId bör innehålla det nya ID:t för författaren
+
+    const userId = user.insertId || user[0].id;
+
+    // kör frågan för att skapa ett nytt inlägg
+    const [rows] = await promisePool.query('INSERT INTO al04forum (authorid, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
 
 router.get('/new', async function (req, res, next) {
